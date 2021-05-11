0a3435fc6c0a1dbf17a0b3a3f73828a3ae5e2d36
Nikolaj Jensen <nj@grouplogistic.dk>
2021-05-11 08:32:42 +0200

    chapter1, done

--- assignments/firstgame/get_number.rb
+++ assignments/firstgame/get_number.rb
@@ -2,7 +2,7 @@
 puts "Welcome to 'Get My Number!'"
 
 # Get the player's name and greet them.
-print "What's your name? \n"
+puts "What's your name? \n"
 
 input = gets
 name = input.chomp
@@ -15,7 +15,32 @@ puts "Can you guess it?"
 
 target = rand(100) + 1
 
+# Track how many guesses the player has made
 num_guesses = 0
 
-remaining_guesses = 10 - num_guesses
-puts "You've got #{10 - num_guesses} guesses left."
+# Track whether the player has guessed correctly
+guessed_it = false
+
+while num_guesses < 10 && guessed_it == false
+
+  puts "You've got #{10 - num_guesses} guesses left."
+
+  print "Make a guess: "
+  guess = gets.to_i
+
+  num_guesses += 1
+
+  if guess < target
+    puts "Oops, your guess was too low."
+  elsif guess > target
+    puts "Oops, your guess was too high."
+  elsif guess == target
+    puts "Good job, #{name}"
+    puts "You guessed my number in #{num_guesses} guesses!"
+    guessed_it = true
+  end
+end
+
+unless guessed_it
+  puts "Sorry, you didn't guess my number. (It was #{target})"
+end
